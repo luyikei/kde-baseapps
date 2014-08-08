2ac4cf9faadbd36856ea2ff4aa851bf8aaa53073
luyikei <luyikei.qmltu@gmail.com>
2014-08-08 23:29:52 +0900

    remove license and useless includes

--- konq-plugins/pi/pi.cpp
+++ konq-plugins/pi/pi.cpp
@@ -1,34 +1,10 @@
-/* This file is part of the KDE project
-   Copyright (C) 2000 David Faure <faure@kde.org>
-
-   This library is free software; you can redistribute it and/or
-   modify it under the terms of the GNU Library General Public
-   License version 2 as published by the Free Software Foundation.
-
-   This library is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
-
-   You should have received a copy of the GNU Library General Public License
-   along with this library; see the file COPYING.LIB.  If not, write to
-   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.
-*/
-
 #include "pi.h"
 #include <kparts/part.h>
-#include <kicon.h>
 #include <kactioncollection.h>
-#include <kinputdialog.h>
 #include <klocale.h>
 #include <kmessagebox.h>
-#include <kshell.h>
 #include <kapplication.h>
 #include <kpluginfactory.h>
-#include <kauthorized.h>
-#include <kio/netaccess.h>
-#include <kparts/fileinfoextension.h>
 
 Pi::Pi( QObject* parent, const QVariantList & )
     : KParts::Plugin( parent )
